6787f04508bd1f41016cd3993ab2392faaeaf514
andrew-k-stayprivate <45094966+andrew-k-stayprivate@users.noreply.github.com>
2019-12-16 14:52:40 +0000

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import SignInForm from './SignInForm';
+import SignUpForm from './SignUpForm';
+Amplify.configure(aws_exports);
 
-import { withAuthenticator } from 'aws-amplify-react'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signedUp: false
+    }
+    this.handleSignup = this.handleSignup.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  handleSignup() {
+    this.setState({
+      signedUp: true
+    });
+  }
+  render() {
+    const { signedUp } = this.state;
+    return !signedUp ? <SignUpForm handleSignup={this.handleSignup} /> : <SignInForm />;
+  }
 }
 
-export default withAuthenticator(App, {
-  includeGreetings: true
-})
+export default App;
